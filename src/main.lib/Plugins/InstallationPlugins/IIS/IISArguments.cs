e26753c8caadb547ebe0bc72cd976cbea3db1589
Daniel <daniel@cbc.nl>
2025-04-30 10:47:57 +0200

    Remove unnecessary name definition and update flag description

--- src/main.lib/Plugins/InstallationPlugins/IIS/IISArguments.cs
+++ src/main.lib/Plugins/InstallationPlugins/IIS/IISArguments.cs
@@ -8,7 +8,6 @@ namespace PKISharp.WACS.Plugins.InstallationPlugins
     {
         private const string SslPortParameterName = "sslport";
         private const string SslIpParameterName = "sslipaddress";
-        private const string UpdateOnlyName = "updateonly";
 
         public override string Name => "IIS plugin";
         public override string Group => "Installation";
@@ -26,7 +25,7 @@ namespace PKISharp.WACS.Plugins.InstallationPlugins
         [CommandLine(Name = SslIpParameterName, Description = "IP address to use for newly created HTTPS bindings. Defaults to " + IISClient.DefaultBindingIp + ".")]
         public string? SSLIPAddress { get; set; }
 
-        [CommandLine(Name = UpdateOnlyName, Description = "Create new bindings in IIS.")]
+        [CommandLine(Description = "Flag to indicate whether new bindings should be created. Defaults to " + IISClient.DefaultUpdateOnly + ".")]
         public bool UpdateOnly { get; set; } = IISClient.DefaultUpdateOnly;
     }
 }
